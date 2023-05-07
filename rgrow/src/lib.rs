f019819736102a9604f87c58ba03dd5216598bbd
Constantine Evans <const@costi.eu>
2023-05-07 22:59:06 +0100

    another approach

--- rgrow/src/lib.rs
+++ rgrow/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(associated_type_bounds)]
+//#![feature(associated_type_bounds)]
 //#![feature(map_first_last)]
 
 //! The rgrow (FIXME: we need a better name!) tileset simulator.  This generally uses the algorithms of the venerable Xgrow.
