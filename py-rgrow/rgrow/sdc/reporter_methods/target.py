11b86d47814b0282473feaf0bb4edee4a9fe0f68
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-09-09 00:34:02 +0100

    target reporter method improved

--- py-rgrow/rgrow/sdc/reporter_methods/target.py
+++ py-rgrow/rgrow/sdc/reporter_methods/target.py
@@ -4,11 +4,23 @@ from ..anneal import AnnealOutputs
 import numpy as np
 
 
+def _snapshot_value(target, outs) -> np.float64:
+    matching = np.all(outs == target, axis=1)
+    percentage = np.sum(matching) / (len(outs) - 4)
+    return percentage
+
+
 class Target(ReportingMethod):
     """
     Reporting method:
 
-    Check if the scaffold matches some array exactly
+    Given some array of tile names, check what percentage of scaffolds have 
+    exactly that configuration.
+
+    For example, if we have an sdc system of length two, and we use the repoorter
+    method Target(["0A0", "1B1"]), then we will see what percentage of the
+    scaffolds had the strand named "0A0" on the input domain, and the strand named
+    "1B1" in the domain to its right.
     """
 
     desc = "Target"
@@ -17,26 +29,12 @@ class Target(ReportingMethod):
         self.target_names = target_names
 
     def reporter_method(self, anneal_outp: AnnealOutputs):
-        target_ids = np.array(
-            (
-                [0, 0]
-                + [
-                    anneal_outp.system.rgrow_system.tile_number_from_name(name)
-                    for name in self.target_names
-                ]
-                + [0, 0]
-            )
-        )
+        system = anneal_outp.system
+        target_ids = np.array([0, 0] + [system.tile_number_from_name(
+            name) for name in self.target_names] + [0, 0])
 
         target_percentage = np.zeros(len(anneal_outp.canvas_arr))
-        i = 0
-        for snapshot in anneal_outp.canvas_arr:
-            correct_target = 0
-            for scaffold in snapshot:
-                if (scaffold == target_ids).all():
-                    correct_target += 1
-
-            target_percentage[i] = correct_target / len(snapshot)
-            i += 1
+        for i, snapshot in enumerate(anneal_outp.canvas_arr):
+            target_percentage[i] = _snapshot_value(target_ids, snapshot)
 
         return target_percentage
