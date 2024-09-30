3f12c3b5e0533f919f235e7de4a1bfe4c3e6c589
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2024-09-30 19:14:31 +0100

    refactor

--- rgrow/src/models/kcov.rs
+++ rgrow/src/models/kcov.rs
@@ -1,8 +1,14 @@
-use crate::type_alias;
+use ndarray::Array2;
+use serde::{Deserialize, Serialize};
 
-type_alias!(
-    f64 => Concentration
-);
+use crate::{
+    base::{Glue, Tile},
+    system::{System, TileBondInfo},
+    type_alias,
+};
+
+type_alias!( f64 => Concentration );
+type_alias!( u32 => TileId );
 
 const NORTH: u32 = 0b1000;
 const SOUTH: u32 = 0b0100;
@@ -11,46 +17,37 @@ const EAST: u32 = 0b0010;
 const WEST: u32 = 0b0001;
 
 const ALL_COVERS: u32 = 0b1111;
+const NO_COVERS: u32 = !ALL_COVERS;
 
-/// Structure for tile with covers
-pub struct CovTile {
-    /// The last four binary digits are reserved for cover status -- This will allow us to quickly
-    /// find the id of a tile that is identical, but with/without a cover
-    id: u32,
-}
+/// Helper methods for tile id
+///
+/// This can help change the id to attach a cover on the north position, etc ...
+mod tileid_helper {
+    use super::{TileId, ALL_COVERS, EAST, NORTH, SOUTH, WEST};
 
-impl CovTile {
-    pub fn new(id: u32) -> Self {
-        Self { id }
+    pub fn attachments(id: TileId) -> TileId {
+        id & ALL_COVERS
     }
 
-    pub fn attachments(&self) -> u32 {
-        self.id & ALL_COVERS
+    pub fn attach_south(id: TileId) -> TileId {
+        id | SOUTH
     }
 
-    pub fn attach_south(&mut self) {
-        self.id |= SOUTH;
+    pub fn attach_north(id: TileId) -> TileId {
+        id | NORTH
     }
 
-    pub fn attach_north(&mut self) {
-        self.id |= NORTH;
+    pub fn attach_east(id: TileId) -> TileId {
+        id | EAST
     }
 
-    pub fn attach_east(&mut self) {
-        self.id |= EAST;
-    }
-
-    pub fn attach_west(&mut self) {
-        self.id |= WEST;
-    }
-
-    pub fn id(&self) -> u32 {
-        self.id
+    pub fn attach_west(id: TileId) -> TileId {
+        id | WEST
     }
 
     /// Get the "base id", this is the id of the tile if it had no covers
-    pub fn base_id(&self) -> u32 {
-        self.id & (!ALL_COVERS)
+    pub fn base_id(id: TileId) -> TileId {
+        id & (!ALL_COVERS)
     }
 }
 
@@ -64,20 +61,13 @@ struct KCov {
 
 #[cfg(test)]
 mod test_covtile {
-    use crate::models::kcov::EAST;
-
-    use super::CovTile;
-
-    fn test_tile() -> CovTile {
-        CovTile::new(0b10110000)
-    }
+    use crate::models::kcov::{tileid_helper, EAST};
 
     #[test]
     fn get_ids() {
-        let mut t = test_tile();
-        t.attach_east();
-
-        assert_eq!(t.base_id(), 0b10110000);
-        assert_eq!(t.id(), 0b10110000 | EAST);
+        let mut t = 0b10110000;
+        t = tileid_helper::attach_east(t);
+        assert_eq!(tileid_helper::base_id(t), 0b10110000);
+        assert_eq!(t, 0b10110000 | EAST);
     }
 }
