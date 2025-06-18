845ea1d3dce6ae715bf0e61bb12e7895a790b2ed
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-06-18 15:47:08 +0100

    A (much) better way to graph!

--- py-rgrow/rgrow/sdc/graphs.py
+++ py-rgrow/rgrow/sdc/graphs.py
@@ -1,136 +1,62 @@
-from typing import Iterable, Sequence
-from .anneal import Anneal, AnnealOutputs
-from .sdc import SDC
+from pathlib import Path
+from .anneal import AnnealOutputs
 from .reporter_methods import ReportingMethod
 
 import matplotlib.pyplot as plt
-
 
 MIN = 60
 HOUR = MIN * 60
 
 
-def graph_system(
-    system: SDC,
-    anneal_output: AnnealOutputs,
-    method: ReportingMethod,
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-):
-    measurement = method.reporter_method(anneal_output)
+class Graphing:
+    def __init__(self, title: str):
+        self.title = title
+        self.fig, self.ax1 = plt.subplots()
+        self.ax2 = self.ax1.twinx()
+        self._times = None
+        self._temps = None
 
-    times, temps = anneal_output.anneal.gen_arrays()
-    times_hours = times / HOUR
+    def add_line(self, anneal_output: AnnealOutputs, method: ReportingMethod, *args, **kwargs):
+        """
+        Adds a line to the graph using a reporting function to extract data.
 
-    plt.clf()
-
-    plt.plot(times_hours, measurement, label=system.name)
-    plt.xlabel("Time (hours)")
-    plt.ylabel(method.desc)
-    plt.ylim(0.0, 1.1)
-
-    plt.legend()
-
-    # Now plot the temperature
-    plt2 = plt.twinx()
-    plt2.plot(times_hours, temps, "k--", label="temperature C")  # type: ignore
-    plt2.set_ylabel("temperature")
-
-    plt.savefig(path)
-
-
-def graph_system_with_many_reporting_methods(
-    system: SDC,
-    anneal_output: AnnealOutputs,
-    methods: list[ReportingMethod],
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-):
-    plt.clf()
-    times, temps = anneal_output.anneal.gen_arrays()
-    times_hours = times / HOUR
-
-    for method in methods:
+        Parameters
+        ----------
+        Todo
+        """
         measurement = method.reporter_method(anneal_output)
-        plt.plot(times_hours, measurement, label=method.desc)
+        times, temps = anneal_output.anneal.gen_arrays()
+        times_hours = times / HOUR
 
-    plt.xlabel("Time (hours)")
-    plt.ylabel("Method Depended Error")
-    plt.ylim(0.0, 1.1)
+        print("Hurs", len(times_hours))
 
-    plt.legend()
+        if self._times is None:
+            self._times = times_hours
+            self._temps = temps
+        elif not (len(times_hours) == len(self._times) and all(times_hours == self._times)):
+            raise ValueError("Time arrays do not match across added lines.")
 
-    # Now plot the temperature
-    plt2 = plt.twinx()
-    plt2.plot(times_hours, temps, "k--", label="temperature C")  # type: ignore
-    plt2.set_ylabel("temperature")
+        self.ax1.plot(times_hours, measurement, *args, **kwargs)
+        self.ax1.set_ylabel(method.desc)
+        self.ax1.set_ylim(0.0, 1.1)
 
-    plt.title(system.name)
+    def end(self, save_path: str | None = None):
+        """
+        Finalizes and renders or saves the plot.
+        """
+        self.ax1.set_title(self.title)
+        self.ax1.set_xlabel("Time (hours)")
+        self.ax1.legend()
+        self.ax1.grid(True)
 
-    plt.savefig(path)
+        # Plot temperature on right-hand axis
+        if self._temps is not None:
+            self.ax2.plot(self._times, self._temps, "k--", label="Temperature (°C)")
+            self.ax2.set_ylabel("Temperature (°C)")
 
-
-def run_and_graph_system(
-    system: SDC,
-    anneal: Anneal,
-    method: ReportingMethod,
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-):
-    graph_system(system, system.run_anneal(anneal), method, path)
-
-
-def run_and_graph_system_with_many_reporting_methods(
-    system: SDC,
-    anneal: Anneal,
-    methods: list[ReportingMethod],
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-):
-    graph_system_with_many_reporting_methods(
-        system, system.run_anneal(anneal), methods, path
-    )
-
-
-def graph_many_systems_with(
-    outputs: Sequence[AnnealOutputs],
-    method: ReportingMethod,
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-    title: str | None = None,
-):
-    plt.clf()
-    times, temps = outputs[0].anneal.gen_arrays()
-    times_hours = times / HOUR
-
-    for o in outputs:
-        measurement = method.reporter_method(o)
-        plt.plot(times_hours, measurement, label=o.system.name)
-
-    plt.xlabel("Time (hours)")
-    plt.ylabel(method.desc)
-    plt.ylim(0.0, 1.1)
-
-    plt.legend()
-
-    # Now plot the temperature
-    plt2 = plt.twinx()
-    plt2.plot(times_hours, temps, "k--", label="temperature C")  # type: ignore
-    plt2.set_ylabel("temperature")
-
-    if title is not None:
-        plt.title(title)
-
-    plt.savefig(path)
-
-
-def run_and_graph_many_systems_with(
-    systems: Iterable[SDC],
-    anneal: Anneal,
-    method: ReportingMethod,
-    # TODO: Fix this for windows
-    path: str = "/tmp/sdc_image.png",
-    title: str | None = None,
-):
-    outs = [s.run_anneal(anneal) for s in systems]
-    graph_many_systems_with(outs, method, path, title)
+        if save_path:
+            Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+            self.fig.savefig(save_path)
+            print(f"[INFO] Saved plot to: {save_path}")
+        else:
+            plt.show()
