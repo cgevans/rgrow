35b44d300979ba8f61dfa63388bf86d50e781f43
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2025-12-31 14:18:53 +0000

    Turn tile to pixels

--- rgrow/src/painter.rs
+++ rgrow/src/painter.rs
@@ -6,9 +6,14 @@ use crate::colors::Color;
 /// it will be treated as 4 triangles rather than one square. The colors provided are
 /// for the NSEW triangles, in that order
 #[derive(Debug, Clone, Copy)]
-struct TileStyle {
+pub struct TileStyle {
     /// RGBA values for each of the sides of a tile
     tri_colors: [Color; 4],
+}
+
+pub struct SpriteSquare {
+    pub size: usize,
+    pub pixels: Box<[u8]>,
 }
 
 impl TileStyle {
@@ -31,11 +36,43 @@ impl TileStyle {
     pub fn west_color(&self) -> Color {
         self.tri_colors[3]
     }
+
+    /// Generate the colors for the tile
+    pub fn as_sprite(&self, size: usize) -> SpriteSquare {
+        let mut pixels = vec![0; size * size * 4];
+        for row in 0..size {
+            for col in 0..size {
+                // North or east
+                let ne = col > row;
+                let se = row > size - col - 1;
+                let color = match (ne, se) {
+                    (true, true) => self.east_color(),
+                    (true, false) => self.north_color(),
+                    (false, true) => self.south_color(),
+                    (false, false) => self.west_color(),
+                };
+                let idx = 4 * (row * size + col);
+                pixels[idx..idx + 4].copy_from_slice(color.as_slice());
+            }
+        }
+
+        SpriteSquare {
+            size,
+            pixels: pixels.into_boxed_slice(),
+        }
+    }
 }
 
 /// Let a `System` decide how the DNA should be painted in the GUI. This trait needs to be
 /// implemented in order for the system to be usable with the GUI.
-trait TilePainter<TileId> {
+pub trait TilePainter {
+    type TileId;
+
     /// Given some tile id, choose how it should be rendered in the GUI
-    fn tile_style(&self, id: TileId) -> TileStyle;
+    fn tile_style(&self, id: Self::TileId) -> TileStyle;
+
+    /// Turn the tile into a sprite
+    fn tile_pixels(&self, id: Self::TileId, size: usize) -> SpriteSquare {
+        self.tile_style(id).as_sprite(size)
+    }
 }
